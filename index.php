765d64ee29af781de82c60637787db4da86fe34d
Claudio <claudiodicaprio89@gmail.com>
2022-01-31 15:20:42 +0100

    init_

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+ require_once __DIR__ . '/classes/User.php';
+ require_once __DIR__ . '/classes/Product.php';
+ require_once __DIR__ . '/classes/CreditCard.php';
+
+
+$new_user = new User();
+
+$new_product = new Product();
+
+$new_creditCard = new CreditCard();
+
+?>
