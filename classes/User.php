d8506c1b1159db836e252b1cd70fb1b6fc1f42bc
Claudio <claudiodicaprio89@gmail.com>
2022-01-31 15:38:35 +0100

    user_properties_setted

--- classes/User.php
+++ classes/User.php
@@ -1,5 +1,43 @@
 <?php
 
-class User{}
+class User{
+    private $name;
+    private $sur;
+    private $item;
+    private $creditCard;
+    protected $discount;
 
+
+//constructor
+    function __construct($_name, $_sur)
+    {
+        $this->firstname = $_name;
+        $this->sur = $_sur;
+    }
+
+//constructor-end
+
+//set properties
+    public function setName($_name) 
+    {
+        $this->name = $_name;
+    }
+    public function setSur($_sur) 
+    {
+        $this->sur = $_sur;
+    }
+    public function gotItem($_item) 
+    {
+        $this->item = $_item;
+    }
+    public function setCreditCard($_creditCard) 
+    {
+        $this->creditCard = $_creditCard;
+    }
+    public function setDiscount($_discount) 
+    {
+        $this->discount = $_discount;
+    }
+//set properties
+}
 ?>
--- index.php
+++ index.php
@@ -5,11 +5,12 @@
  require_once __DIR__ . '/classes/CreditCard.php';
 
 
-$new_user = new User();
-
+$new_user = new User('Mace','Windu');
+// var_dump($new_user);
 $new_product = new Product();
 
 $new_creditCard = new CreditCard();
+
 
 ?>
 
