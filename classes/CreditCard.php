f1f842ae2d6bdbad1532e80202eac4dc71e83cd0
Claudio <claudiodicaprio89@gmail.com>
2022-01-31 16:00:39 +0100

    CreditCard_constructor

--- classes/CreditCard.php
+++ classes/CreditCard.php
@@ -1,5 +1,27 @@
 <?php
 class CreditCard{
-    
+    private $cardNumber;
+    private $cvv;
+    private $exYear;
+    private $exMonth;
+
+//constructor
+    function __construct($_cardNumber, $_cvv, $_exYear, $_exMonth)
+    {
+        $this->cardNumber = $_cardNumber;
+        $this->cvv = $_cvv;
+        $this->exYear = $_exYear;
+        $this->exMonth = $_exMonth;    
+    }
+//constructor end
+
+//set properties
+
+//set properties end
+
+//get properties 
+
+//get properties end
+
 }
 ?>
