d70cd324e325ebd89f7db8f7e5a50071e030feb6
Claudio <claudiodicaprio89@gmail.com>
2022-01-31 16:22:11 +0100

    no_sur

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
 
 
 $new_user = new User('Mace','Windu');
-// var_dump($new_user);
+
 $new_product = new Product('Single blade Lightsaber"Amethyst"',28000);
 
 $new_creditCard = new CreditCard(5333271098172363, 932, 24, 01);
 
-
+$new_user->setCreditCard($new_creditCard);
+$new_user->gotItem($new_product);
+// var_dump($new_user);
 ?>
 
 <!DOCTYPE html>
@@ -20,9 +22,11 @@ $new_creditCard = new CreditCard(5333271098172363, 932, 24, 01);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHOP</title>
+    <title>THE FORCE shop</title>
 </head>
 <body>
-    
+    <h2> Sales Report</h2>
+    <p> <?php echo"Mister:{$new_user->getName()} {$new_user->getSur()}"?></p>
+    <p>Pur</p>
 </body>
 </html>
